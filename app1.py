de06f7f7acba8db8d83d9e8f762915cbdd000935
davin <davinfalahtama@gmail.com>
2024-03-19 08:56:35 +0700

    hapus lib

--- app1.py
+++ app1.py
@@ -71,21 +71,6 @@ def user_input(user_question, pdf_text):
     st.write("Reply: ", response["output_text"])
 
 
-
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
-import random
-
-
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
