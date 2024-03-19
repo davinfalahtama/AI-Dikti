7338ba25ba3cb1a71355422cee662e896a2156aa
Alex Januar <alexanderjanuar16@gmail.com>
2024-03-19 11:42:43 +0800

    Update codebase to use instruction-tuned and RLHF-tuned language models.

--- app1.py
+++ app1.py
@@ -11,6 +11,11 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import random
 import time
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+import pandas as pd
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
@@ -18,12 +23,18 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_pdf_text(pdf_docs):
     text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+    for file in pdf_docs:
+        if file.name.endswith('.pdf'):
+            # Process PDF file
+            pdf_reader = PdfReader(file)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        elif file.name.endswith('.csv'):
+            # Process CSV file
+            df = pd.read_csv(file)
+            for index, row in df.iterrows():
+                text += row['facts']
     return text
-
 
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
@@ -36,6 +47,31 @@ def get_vector_store(text_chunks):
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+def contextualize_system_prompt():
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
+    )
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5,convert_system_message_to_human=True)
+    contextualize_q_chain = contextualize_q_prompt | model | StrOutputParser()
+    return contextualize_q_chain
+
+def contextualized_question(input: dict):
+    if input.get("chat_history"):
+        contextualize_q_chain = contextualize_system_prompt()
+        return contextualize_q_chain
+    else:
+        return input["question"]
 
 def get_conversational_chain():
     prompt_template = """
@@ -46,38 +82,43 @@ def get_conversational_chain():
         Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.5,convert_system_message_to_human=True)
 
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
 
-    return chain
+    retriever = new_db.as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
-
-def user_input(user_question, pdf_text):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(pdf_text)  # Use text from PDF as context
-
-    chain = get_conversational_chain()
-
-    response = chain(
-        {"input_documents": docs, "question": user_question},
-        return_only_outputs=True
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt_template),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
     )
 
-    return response["output_text"]
+    rag_chain = (
+        RunnablePassthrough.assign(
+            context=contextualized_question | retriever | format_docs
+        )
+        | qa_prompt
+        | model
+    )    
+    return rag_chain
 
+def response_generator(text):
+    for word in text.split():
+        yield word + " "
+        time.sleep(0.05)
 
 def main():
     st.set_page_config("Chat PDF")
     st.title("Simple chat")
-
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
+    
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
+    
     with st.sidebar:
         st.title("Menu:")
         pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button",
@@ -93,25 +134,27 @@ def main():
             else:
                 st.error("Please upload PDF files first.")
 
+    for message in st.session_state["chat_history"]:
+        if isinstance(message, HumanMessage):
+            with st.chat_message("user"):
+                st.markdown(message.content)
+        elif isinstance(message, AIMessage):
+            with st.chat_message("assistant"):
+                st.markdown(message.content)
     # Accept user input
     if prompt := st.chat_input("Say something"):
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
-
-        # Get text from uploaded PDF files if pdf_docs is not None
+        
         if pdf_docs:
-            # Get text from uploaded PDF files
-            pdf_text = get_pdf_text(pdf_docs)
-            # Pass user input and PDF text to user_input function
-            answer = user_input(prompt, pdf_text)
-            # Display assistant response in chat message container
+            rag_chain = get_conversational_chain()
+            ai_msg = rag_chain.invoke({"question": prompt, "chat_history": st.session_state["chat_history"]})
+
             with st.chat_message("assistant"):
-                st.write(answer)
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": answer})
+                response = st.write_stream(response_generator(ai_msg.content))
+
+            st.session_state["chat_history"].extend([HumanMessage(content=prompt), ai_msg])
+            
         else:
             st.error("Please upload PDF files first.")
 
